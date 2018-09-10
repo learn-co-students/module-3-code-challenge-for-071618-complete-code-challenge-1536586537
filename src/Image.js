890fe1a8ec51b27b81ac45be3ce700c057877e24
chejianchao <820992376@qq.com>
2018-09-10 10:57:53 -0400

    Done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,50 @@
-class Image {
-  
+const commentul = document.getElementById("comments")
 
+class Image {
+
+  constructor(url){
+    this.like_count = 0
+    this.comments = []
+    fetch(url)
+      .then(resp=>resp.json())
+      .then(json=>{
+        const imageIMG = document.getElementById("image")
+        imageIMG.src = json.url
+        this.id = json.id
+        json.comments.sort((a,b)=>{
+          return a.id-b.id
+        })
+        this.comments = json.comments
+        this.renderComment()
+        this.addLike(json.like_count)
+      })
+
+  }
+
+  addComment(comment){
+    this.comments.push(comment)
+    this.renderComment()
+  }
+  renderComment(){
+    commentul.innerHTML = ""
+
+    this.comments.forEach(comment=>{
+      const li = document.createElement("li")
+      li.innerHTML = comment.content + `<button data-commentid=${comment.id}>X</button>`
+      commentul.append(li)
+    })
+  }
+  addLike(count){
+    let likeSpan = document.getElementById("likes")
+    this.like_count+=count
+    likeSpan.innerHTML =this.like_count
+
+  }
+
+  deleteComment(id){
+    this.comments = this.comments.filter(comment=>{
+      return comment.id !== parseInt(id)
+    })
+    this.renderComment()
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = parseInt(Math.random()*8)+1 //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -8,7 +8,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  let image = new Image(imageURL)
+  contentText = document.getElementById("comment_input")
+  comment_form = document.getElementById("comment_form")
+  comment_form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if( contentText.value === "" )
+      return alert("Please input comment.")
+    const data = {
+      method:"POST",
+      body:JSON.stringify({
+        image_id:image.id,
+        content:contentText.value,
+      }),
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+    fetch(commentsURL, data)
+      .then(resp=>resp.json())
+      .then(json=>{
+        image.addComment(json)
+      })
+  })
 
 
+  likeButton = document.getElementById("like_button")
+  like_button.addEventListener("click", (e)=>{
+    const data = {
+      method:"POST",
+      body:JSON.stringify({
+        image_id:image.id,
+      }),
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+    fetch(likeURL, data)
+      .then(resp=>resp.json())
+      .then(json=>{
+        image.addLike(1)
+      })
+  })
+
+  const commentul = document.getElementById("comments")
+  commentul.addEventListener("click", (e)=>{
+      let commentid = e.target.dataset.commentid
+      if( commentid ){
+        const data = {
+          method:"DELETE",
+        }
+        fetch(commentsURL+commentid, data)
+          .then(resp=>resp.json())
+          .then(json=>{
+            image.deleteComment(commentid)
+          })
+
+      }
+  })
 })
-
